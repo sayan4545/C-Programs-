c21cb436fdb80eafa8e4bb37253e85415e78d240
Sayan Chatterjee <70452470+sayan4545@users.noreply.github.com>
2022-12-12 23:59:54 +0530

    Add files via upload
    Since I am just starting out. This is my first code in C . And its not "Hello World".

---
+++ area_of_a_square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float a, area;
+    printf("Enter the value of the side of the square: ");
+    scanf("%f",&a);
+    area = a*a;
+    printf("The area of the square is %f",area);
+    return 0;
+    
+}
